2c74c0e339fba096e803af287ef633ce36bab90d
Mudasir Mujtaba <mudasirmujtaba15@gmail.com>
2025-05-04 00:47:25 +0500

    async handler added

--- backened/controllers/entryControllers.js
+++ backened/controllers/entryControllers.js
@@ -1,18 +1,27 @@
-const getEntry = (req, res)=>{
+const asyncHandler = require("express-async-handler")
+
+const getEntry = asyncHandler(async (req, res)=>{
     res.status(200).json({message: "Get Entry",})
-}
+})
 
-const setEntry = (req, res)=>{
+const setEntry = asyncHandler (async(req, res)=>{
+
+    if(!req.body.text){
+        res.status(400)
+        throw new Error("Please add a text field")
+    }
+
+
     res.status(200).json({message: "Set Entry",})
-}
+})
 
-const updateEntry = (req, res)=>{
+const updateEntry = asyncHandler(async(req, res)=>{
     res.status(200).json({message: `Update Goal ${req.params.id}`,})
-}
+})
 
-const deleteEntry = (req, res)=>{
+const deleteEntry =asyncHandler( async (req, res)=>{
     res.status(200).json({message: `Delete Goal ${req.params.id}`,})
-}
+})
 
 
 module.exports = {
--- backened/server.js
+++ backened/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config()
+const {errorHandler}= require("./middlewares/errorHandlerMiddleware.js")
 
 const port = process.env.Port || 5000
 
@@ -11,6 +12,8 @@ app.use(express.urlencoded({ extended: false }));
 
 
 app.use("/api/entries",require("./routes/entryRoutes.js"))
+
+app.use(errorHandler)
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
