b3dad4d5e11b517a679076d87ebc7ca386b2109c
Mudasir Mujtaba <mudasirmujtaba15@gmail.com>
2025-05-10 17:47:19 +0500

    backened part completed with user and entry association for athetication and authorizaton

--- backened/controllers/entryControllers.js
+++ backened/controllers/entryControllers.js
@@ -1,10 +1,13 @@
 const asyncHandler = require("express-async-handler")
-const Entry = require("../models/entryModel.js")
+const entryModel = require("../models/entryModel.js")
+const userModel = require("../models/userModel.js")
 
 
 const getEntry = asyncHandler(async (req, res)=>{
 
-    const entries = await Entry.find()
+    // getting specific user entries
+
+    const entries = await entryModel.find({user:req.user.id})
     res.status(200).json(entries)
 
 })
@@ -18,9 +21,10 @@ const setEntry = asyncHandler (async(req, res)=>{
         throw new Error("Please add both title and content fields")
     }
 
-    const entry = await Entry.create({
+    const entry = await entryModel.create({
         title,
         content,
+        user: req.user.id, // associate the entry with the logged-in user
     })
 
     res.status(200).json(entry)
@@ -28,14 +32,27 @@ const setEntry = asyncHandler (async(req, res)=>{
 
 const updateEntry = asyncHandler(async(req, res)=>{
 
-    const entry = await Entry.findById(req.params.id)
+    const entry = await entryModel.findById(req.params.id)
 
     if(!entry){
         res.status(400)
         throw new Error("Entry not found")
     }
 
-    const updatedEntry = await Entry.findByIdAndUpdate(req.params.id, req.body, {
+    const user = await userModel.findById(req.user.id)
+    if (!user) {
+        res.status(401)
+        throw new Error("User not found")
+    }
+
+    
+    // Check if the logged-in user is the owner of the entry
+    if(entry.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error("User not authorized")
+    }
+
+    const updatedEntry = await entryModel.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
         runValidators: true,
     })
@@ -46,14 +63,27 @@ const updateEntry = asyncHandler(async(req, res)=>{
 
 const deleteEntry =asyncHandler( async (req, res)=>{
 
-    const entry = await Entry.findById(req.params.id)
+    const entry = await entryModel.findById(req.params.id)
 
     if(!entry){
         res.status(400)
         throw new Error("Entry not found")
     }
 
-    await Entry.findByIdAndDelete(req.params.id)
+     const user = await userModel.findById(req.user.id)
+    if (!user) {
+        res.status(401)
+        throw new Error("User not found")
+    }
+
+    
+    // Check if the logged-in user is the owner of the entry
+    if(entry.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error("User not authorized")
+    }
+
+    await entryModel.findByIdAndDelete(req.params.id)
     res.status(200).json({message: `Delete Goal ${req.params.id}`,})
 })
 
