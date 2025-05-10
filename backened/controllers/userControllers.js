61d2a619a4efd518556870d8ff08b0da61bea224
Mudasir Mujtaba <mudasirmujtaba15@gmail.com>
2025-05-10 12:56:23 +0500

    bcrypt and jsontoken packages added in user controllers

--- backened/controllers/userControllers.js
+++ backened/controllers/userControllers.js
@@ -1,20 +1,35 @@
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+const asyncHandler = require("express-async-handler")
+const userModel = require("../models/userModel")
+
 
 // route /api/users
 // public
-const registerUser = async (req,res)=>{
+const registerUser = asyncHandler(async (req,res)=>{
+
+    const {name , email, password }= req.body
+     
+    if(!name || !email || !password){
+        res.status(400)
+        throw new Error("Please add all fields")
+    }
+
+
+
     res.json({message:"register user"})
-}
+})
 
 // route /api/users/login
-const loginUser = async (req, res)=>{
+const loginUser = asyncHandler(async (req, res)=>{
     res.json({message:"loogin user"})
-}
+})
 
 // route /api/users/me
 // private
- const getMe = async (req,res)=>{
+ const getMe = asyncHandler(async(req,res)=>{
     res.json({message:"user data"})
- }
+ })
 
 module.exports={
     registerUser,
