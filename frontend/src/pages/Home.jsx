b43d0fe1d152b3ee85d52ea604d778e8211de402
Mudasir Mujtaba <mudasirmujtaba15@gmail.com>
2025-06-03 22:26:19 +0500

    navbar added in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,13 @@
-import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
-    <div className="min-h-screen bg-[#1e1e1e] text-white flex flex-col items-center justify-center gap-6">
-  <h1 className="text-4xl font-bold text-red-500 text-center">Welcome to Daily Journal App</h1>
-  <Link
-    to="/dashboard"
-    className="bg-red-600 text-white px-6 py-3 rounded shadow hover:bg-red-700 transition duration-300"
-  >
-    Go to Dashboard
-  </Link>
-</div>
-
+    <div className="min-h-screen bg-[#1e1e1e] text-white">
+      <Navbar />
+      <div className="flex flex-col items-center justify-center gap-6 h-[80vh]">
+        <h1 className="text-4xl font-bold text-red-500">Welcome to Daily Journal App</h1>
+      </div>
+    </div>
   );
 };
 
