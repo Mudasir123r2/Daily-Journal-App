89daa23ce10e20435026f133db63314ccaf607fb
Mudasir Mujtaba <mudasirmujtaba15@gmail.com>
2025-06-03 23:31:39 +0500

    App file updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider, useAuth } from "./context/AuthContext";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import Dashboard from "./pages/dashboard";
 import NewEntry from "./pages/newEntry";
 import ViewEntry from "./pages/ViewEntry";
 import EditEntry from "./pages/EditEntry";
+import Home from "./pages/home"
+
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/new" element={<NewEntry />} />
-        <Route path="/view/:id" element={<ViewEntry />} />
-        <Route path="/edit/:id" element={<EditEntry />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+          <Route path="/new" element={<PrivateRoute><NewEntry /></PrivateRoute>} />
+          <Route path="/view/:id" element={<PrivateRoute><ViewEntry /></PrivateRoute>} />
+          <Route path="/edit/:id" element={<PrivateRoute><EditEntry /></PrivateRoute>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
