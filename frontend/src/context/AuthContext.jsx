2cac0e61386f79302de431419e3282abe70d5bb0
Mudasir Mujtaba <mudasirmujtaba15@gmail.com>
2025-06-03 22:00:38 +0500

    Authorization Context has been created for login, register,and logout using Context Api

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,46 @@
+import {useContext,useState,useEffect,createContext} from "react"
+import axiosInstance from "../api/axiosInstance"
+
+const AuthContext = createContext()
+
+// eslint-disable-next-line react/prop-types
+export const AuthProvider = ({children})=>{
+
+    const [user,setUser]=useState(null)
+
+    const login = async (formData)=>{
+        const res = await axiosInstance.post("/auth/login",formData)
+        setUser(res.data.user)
+        localStorage.setItem("token",res.data.token)
+    }
+
+    const register = async (formData)=>{
+        const res = await axiosInstance.post("/auth/login",formData)
+        setUser(res.data.user)
+        localStorage.setItem("token",res.data.token)
+    }
+
+    const logout = ()=>{
+        setUser(null)
+        localStorage.removeItem("token")
+    }
+
+    useEffect(()=>{
+        const token = localStorage.getItem("token")
+        if(token){
+            axiosInstance.get("/auth/me").then((res)=>{
+                setUser(res.data)
+            })
+        }
+    },[])
+
+
+    return (
+        <AuthContext.Provider value={{user,login,register,logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuth = ()=> useContext(AuthContext)
