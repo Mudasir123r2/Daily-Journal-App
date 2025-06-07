27b878bcefb50d626c0df277948adec0e866c176
Mudasir Mujtaba <mudasirmujtaba15@gmail.com>
2025-06-08 00:31:44 +0500

    bug fixing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,12 @@ import { AuthProvider, } from "./context/AuthContext";
 import ProtectedRoute from "./utils/ProtectedRoute";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import NewEntry from "./pages/NewEntry";
+import Dashboard from "./pages/dashboard";
+import NewEntry from "./pages/newEntry";
 import ViewEntry from "./pages/ViewEntry";
 import EditEntry from "./pages/EditEntry";
 import DeleteEntry from "./pages/DeleteEntry";
-import Home from "./pages/Home"
-
+import Home from "./pages/home"
 
 
 function App() {
